cf74e720278250b3d1d3d7bb6828695402571ea3
erdgnerdgn <erdogan.saban2020@gmail.com>
2024-07-16 09:56:32 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+document.write('hi everybody'+ "</br>") 
+console.log(" you can find your mistake here")
+// alert("watch out")
+
+var adSoyad = "ahmet mehmet"
+console.log(adSoyad);
+document.write(adSoyad + "</br>")
+
+adSoyad = "mehmet ahmet"
+document.write(adSoyad + "</br>")
+const PI = 3.14
+document.write("PI : " +  PI + "</br>")
+
+let arabaAdi = "Volvo"
+document.write("araba adi: " + arabaAdi + "</br>")
+
+let no = 2345, data = 1979, memleket = "urla" 
+document.write(" numara : " +   no,"</br>" , "dogum tarihi : ", data, "memleket  : ", memleket)
